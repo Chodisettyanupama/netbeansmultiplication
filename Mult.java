17332de20e2f2c70d2f532c8b812244b8247bd63
Chodisettyanupama <91118800+Chodisettyanupama@users.noreply.github.com>
2021-12-03 10:11:44 +0530

    Add files via upload

---
+++ Mult.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author PEC-IT
+ */
+public class Mult 
+{
+    public static void main(String args[])
+    {
+        int a=5;
+        int b=10;
+        int c=a*b;
+        System.out.println("multiplication is "+c);
+    }
+    
+}
